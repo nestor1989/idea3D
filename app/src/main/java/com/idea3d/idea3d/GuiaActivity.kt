8863b41dbcd8ffbcc448cccf41e3c2d49c73bc82
Néstor <69014050+nestor1989@users.noreply.github.com>
2021-08-17 21:05:40 -0300

    ultimo

--- app/src/main/java/com/idea3d/idea3d/GuiaActivity.kt
+++ app/src/main/java/com/idea3d/idea3d/GuiaActivity.kt
@@ -2,15 +2,42 @@ package com.idea3d.idea3d
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.idea3d.idea3d.databinding.ActivityCalcuBinding
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.idea3d.idea3d.databinding.ActivityGuiaBinding
 
 class GuiaActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityGuiaBinding
+
+    val guiaErrores: List<Problemas> = listOf(
+        Problemas("hilos","sale con hilos", "retracción", "picos", "temperatura", "@drawable/logo"),
+        Problemas("warping", "nadaas", "temp0", "dndj", "jdjj", "kdkdk"),
+        Problemas("warping2", "nadaas", "temp0", "dndj", "jdjj", "kdkdk"),
+        Problemas("warping3", "nadaas", "temp0", "dndj", "jdjj", "kdkdk"),
+        Problemas("warping4", "nadaas", "temp0", "dndj", "jdjj", "kdkdk"),
+        Problemas("warping5", "nadaas", "temp0", "dndj", "jdjj", "kdkdk"),
+        Problemas("warping6", "nadaas", "temp0", "dndj", "jdjj", "kdkdk"),
+        Problemas("warping7", "nadaas", "temp0", "dndj", "jdjj", "kdkdk"),
+        Problemas("warping8", "nadaas", "temp0", "dndj", "jdjj", "kdkdk"),
+
+            )
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGuiaBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initAdapter()
+
     }
+
+    fun initAdapter(){
+        binding.recyclerGuia.layoutManager=LinearLayoutManager(this)
+        val adapter = problemasAdapter(guiaErrores)
+        binding.recyclerGuia.adapter = adapter
+
+    }
+
+
 }
---
+++ app/src/main/java/com/idea3d/idea3d/Problemas.kt
@@ -0,0 +1,9 @@
+package com.idea3d.idea3d
+
+data class Problemas(val NombreProblema:String,
+                     val Desripcion:String,
+                     val Resolucion1:String,
+                     val Resolucion2:String,
+                     val Resolucion3:String,
+                     val imagen:String,
+                     )
