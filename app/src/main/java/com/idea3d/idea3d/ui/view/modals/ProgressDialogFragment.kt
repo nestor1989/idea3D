9c28990c9bc2d3d4046ac20aa74dc33fbbbe7468
ihsa-nestor-delrio <ndelrio@emergencias.com.ar>
2023-02-15 13:10:40 -0300

    fixed progress dialog

--- app/src/main/java/com/idea3d/idea3d/ui/view/modals/ProgressDialogFragment.kt
+++ app/src/main/java/com/idea3d/idea3d/ui/view/modals/ProgressDialogFragment.kt
@@ -1,9 +1,12 @@
 package com.idea3d.idea3d.ui.view.modals
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import androidx.fragment.app.DialogFragment
 import com.idea3d.idea3d.databinding.FragmentProgressDialogBinding
 
@@ -22,6 +25,13 @@ class ProgressDialogFragment : DialogFragment() {
     ): View? {
         _binding = FragmentProgressDialogBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        setStyle(DialogFragment.STYLE_NO_FRAME, android.R.style.Theme)
     }
 
     fun newInstance(): ProgressDialogFragment {
