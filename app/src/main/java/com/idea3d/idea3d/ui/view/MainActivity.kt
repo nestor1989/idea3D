b7c6aa234d292e4f79a956ef7b796890a2ebd267
nestor1989 <nestordelrio1989@gmail.com>
2023-04-18 13:00:11 -0300

    fixed dark switch

--- app/src/main/java/com/idea3d/idea3d/ui/view/MainActivity.kt
+++ app/src/main/java/com/idea3d/idea3d/ui/view/MainActivity.kt
@@ -100,9 +100,9 @@ class MainActivity : AppCompatActivity() {
 
     fun setThemes(){
 
-        val darkMode = AppCompatDelegate.getDefaultNightMode()
+        val darkMode = applicationContext.resources.configuration.uiMode
         Log.d("DARK_MODE", darkMode.toString())
-        if (darkMode == AppCompatDelegate.MODE_NIGHT_YES || darkMode == AppCompatDelegate.MODE_NIGHT_UNSPECIFIED) binding.switch1.isChecked = true
+        if (darkMode == 33) binding.switch1.isChecked = true
 
         binding.switch1.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
