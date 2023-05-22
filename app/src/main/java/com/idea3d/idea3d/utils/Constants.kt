10fdff2c8000e9e96e100a6c53691e78632379eb
nestor1989 <nestordelrio1989@gmail.com>
2023-05-22 18:06:15 -0300

    added pay link

--- app/src/main/java/com/idea3d/idea3d/utils/Constants.kt
+++ app/src/main/java/com/idea3d/idea3d/utils/Constants.kt
@@ -5,6 +5,6 @@ class Constants {
         const val API_KEY = "a1c58738e22d4cff88e11303aa537e7e"
         const val THING_KEY = "Bearer 7a75d2e90e63619c17a9ddc68b06980b"
         const val PER_PAGE = 50
-        const val DONATE_LINK = ""
+        const val DONATE_LINK = "https://paypal.me/nestordelrio?country.x=AR&locale.x=es_XC"
     }
 }
