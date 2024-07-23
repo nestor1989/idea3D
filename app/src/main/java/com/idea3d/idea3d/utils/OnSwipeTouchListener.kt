f487ea77d4e257579160c8b0fb061a8fca4a9f19
nestor1989 <nestordelrio1989@gmail.com>
2024-07-23 20:54:57 -0300

    migrated to api level 34

--- app/src/main/java/com/idea3d/idea3d/utils/OnSwipeTouchListener.kt
+++ app/src/main/java/com/idea3d/idea3d/utils/OnSwipeTouchListener.kt
@@ -32,13 +32,13 @@ OnTouchListener {
          super.onLongPress(e)
       }
       override fun onFling(
-         e1: MotionEvent,
+         e1: MotionEvent?,
          e2: MotionEvent,
          velocityX: Float,
          velocityY: Float
       ): Boolean {
          try {
-            val diffY = e2.y - e1.y
+            val diffY = e2.y - e1!!.y
             val diffX = e2.x - e1.x
             if (abs(diffX) > abs(diffY)) {
                if (abs(diffX) > SWIPE_THRESHOLD && abs(
